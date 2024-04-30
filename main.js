9e8d6ded66e7e69942e570ca4620be701231d91a
aykutkaraca <karaca.aykut@gmail.com>
2024-04-30 16:46:11 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,6 @@
  * depends on jQuery>=1.7
  */
 (function() {
-
     /**
      * Returns true if this browser supports canvas
      *
@@ -17,6 +16,21 @@
     /**
      * Handle scratch event on a scratcher
      */
+    function checkpct() {
+        if (pct3>0 && pct4>0 && pct5>0)  {
+            if (pct3<20 || pct4<20 || pct5<20)  {
+
+            //$('#scratcher3Pct').html('It is a girl!');
+            document.getElementById("scratcher3Pct").innerHTML="Scratch MORE!";
+            } 
+        }
+        if (pct3>20 && pct4>20 && pct5>20) {
+            $('#title').html('It is a girl!');
+            document.getElementById("title").style.color = "#FDB3FD";
+            document.getElementById("scratcher3Pct").innerHTML="It is a GIRL!";
+            confetti_effect();
+        }
+    };
     function scratcher3Changed(ev) {
         // Test every pixel. Very accurate, but might be slow on large
         // canvases on underpowered devices:
@@ -24,17 +38,40 @@
     
         // Only test every 32nd pixel. 32x faster, but might lead to
         // inaccuracy:
-        var pct1 = (scratchers[3].fullAmount(32) * 100)|0;
-        var pct2 = (scratchers[4].fullAmount(32) * 100)|0;
-        var pct3 = (scratchers[5].fullAmount(32) * 100)|0;
-        $('#scratcher3Pct').html(' ' + pct1 + '%');
 
-        if (pct1>10 && pct2>10 && pct3>10) {
-                    $('#scratcher3Pct').html('It is a girl!');
-                    document.getElementById("scratcher3Pct").innerHTML="It is a GIRL!";
-        }
+        pct3 = (this.fullAmount(32) * 100)|0;
+        checkpct();
+        
     };
-    
+    function scratcher4Changed(ev) {
+        pct4 = (this.fullAmount(32) * 100)|0;
+        checkpct();
+    };
+    function scratcher5Changed(ev) {
+        pct5 = (this.fullAmount(32) * 100)|0;
+       checkpct();
+    };
+    function randomInRange(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+    function confetti_effect() {
+        var duration = 15 * 1000;
+        var animationEnd = Date.now() + duration;
+        var defaults = { startVelocity: 10, spread: 360, ticks: 60, zIndex: 0 };
+
+        var interval = setInterval(function() {
+        var timeLeft = animationEnd - Date.now();
+
+        if (timeLeft <= 0) {
+            return clearInterval(interval);
+        }
+
+        var particleCount = 50 * (timeLeft / duration);
+        // since particles fall down, start a bit higher than random
+        confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 }, colors: ['#FDB3FD']});
+        confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },colors: ['#FDB3FD'] });
+        }, 250);
+    }
     /**
      * Reset all scratchers
      */
@@ -44,7 +81,11 @@
         for (i = 0; i < scratchers.length; i++) {
             scratchers[i].reset();
         }
-    
+        $("#scratcher3Pct").html('Find the gender');
+        pct3,pct4,pct5=0;
+        $('#title').html('Boy or Girl');
+        document.getElementById("title").style.color = "#000000";
+
         return false;
     };
     
@@ -54,8 +95,9 @@
     function initPage() {
         var scratcherLoadedCount = 0;
         var scratchers = [];
+        var pct3,pct4,pct5=0;
         var i, i1;
-    
+       
         // called each time a scratcher loads
         function onScratcherLoaded(ev) {
             scratcherLoadedCount++;
@@ -95,9 +137,9 @@
         //scratchers[3].addEventListener('reset', scratchersChanged);
         scratchers[3].addEventListener('scratchesended', scratcher3Changed);
         //scratchers[4].addEventListener('reset', scratchersChanged);
-        scratchers[4].addEventListener('scratchesended', scratcher3Changed);
+        scratchers[4].addEventListener('scratchesended', scratcher4Changed);
         //scratchers[5].addEventListener('reset', scratchersChanged);
-        scratchers[5].addEventListener('scratchesended', scratcher3Changed);
+        scratchers[5].addEventListener('scratchesended', scratcher5Changed);
     
         // Or if you didn't want to do it every scratch (to save CPU), you
         // can just do it on 'scratchesended' instead of 'scratch':
