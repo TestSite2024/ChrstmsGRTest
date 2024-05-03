765b4001a82c1de6d0b5875c4e266b396af3beac
aykutkaraca <karaca.aykut@gmail.com>
2024-05-04 00:50:53 +0300

    all changed

--- main.js
+++ main.js
@@ -10,7 +10,8 @@
      * From http://diveintohtml5.info/
      */
     var audio = new Audio('audio/celebrate.mp3');
-
+    var triggered=false;
+    var nosound=true;
     function supportsCanvas() {
         return !!document.createElement('canvas').getContext;
     };
@@ -24,17 +25,25 @@
 
             //$('#scratcher3Pct').html('It is a girl!');
             $("#scratcher3Pct").show();
-            //alert("Scratch MORE!");
             document.getElementById("scratcher3Pct").innerHTML="Scratch MORE!";
             } 
         }
         if (pct3>20 && pct4>20 && pct5>20) {
-            $('#title').html('It is a girl!');
-            document.getElementById("title").style.color = "#FDB3FD";
-            document.getElementById("title").style.fontSize = "16vmin";
+            $('#boy').text('It is a girl!');
+            $('#boy').css('color','#F860AA');
+
+            //document.getElementById("boy").style.color('#F860AA');
+
+            $('#or').hide();
+            $('#girl').hide();
+            //document.getElementById("title").style.color = "#F860AA";
+            document.getElementsByTagName("body")[0].style.backgroundImage = 'repeating-linear-gradient(90deg, #ff95c8, #FFFFFF)';
+            //document.getElementsByTagName("body")[0].style.backgroundImage.animation = 'gradient 15s ease infinite';
+            document.getElementById("title").style.fontSize = "17vmin";
             $('#H3').hide();
             $('#H4').hide();
             $('#scratcher3Pct').hide();
+
             confetti_effect();
         }
     };
@@ -60,32 +69,63 @@
     };
     function randomInRange(min, max) {
         return Math.random() * (max - min) + min;
-    }
+    };
     function confetti_effect() {
-        audio.play();
-
+        if(triggered==true) {
+            return;
+        }
+        if (!nosound) {
+            audio.volume=0.5;
+            audio.play();
+        }
+        triggered=true;
+        // do this for 10 seconds
         var duration = 10 * 1000;
+        var end = Date.now() + duration;
+        var defaults = { startVelocity: 10, spread: 360, ticks: 70, zIndex: 0 };
+        var particleCount = 5;
+        (function frame() {
+        // launch a few confetti from the left edge
+        confetti({...defaults, particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 }, colors: ['#FFFFFF']}
+        );
+        // and launch a few from the right edge
+        confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },colors: ['#FFFFFF']}
+        );
+
+        // keep going until we are out of time
+        if (Date.now() < end) {
+            requestAnimationFrame(frame);
+            
+            return;
+        }
+        console.log("triggered");
+        $("#resetbutton").show();
+        //onResetClicked(scratchers);
+        }());
+        /* var duration = 10 * 1000;
         var animationEnd = Date.now() + duration;
         var defaults = { startVelocity: 10, spread: 360, ticks: 70, zIndex: 0 };
 
-        var interval = setInterval(function() {
+        interval = setInterval(()=> {
         var timeLeft = animationEnd - Date.now();
 
         if (timeLeft <= 0) {
-            console.log("bitti");
+            console.log("triggered");
+
             $("#resetbutton").show();
             //onResetClicked(scratchers);
             audio.stop();
-            return clearInterval(interval);
+            clearInterval(interval);
+            return 
         }
 
         var particleCount = 50 * (timeLeft / duration);
         // since particles fall down, start a bit higher than random
         confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 }, colors: ['#FDB3FD']});
-        confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },colors: ['#FDB3F'] });
-        }, 250);
+        confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },colors: ['#FDB3FD'] });
+        }, 250); */
          
-    }
+    };
     /**
      * Reset all scratchers
      */
@@ -99,14 +139,24 @@
         for (i = 0; i < scratchers.length; i++) {
             scratchers[i].reset();
         }
-        $("#scratcher3Pct").html('Find the gender');
+        //$("#scratcher3Pct").html('Find the gender');
        
-        $('#title').html('Boy or Girl');
-        document.getElementById("title").style.color = "#000000";
+        $('#boy').text('Boy');
+        $('#boy').css('color','#7fffd4');
+        $('#or').show();
+        $('#girl').show();
+
+        //document.getElementById("title").style.color = "#000000";
         document.getElementById("title").style.fontSize = "15vmin";
+
+
+        document.getElementsByTagName("body")[0].style.backgroundImage = 'repeating-linear-gradient(90deg, #ff95c8, #7fffd4)';
+
         $('#H3').show();
         $('#H4').show();
-
+        triggered = false;
+        audio.pause();
+        audio.currentTime = 0;    
         return false;
     };
     
@@ -118,10 +168,15 @@
         var scratchers = [];
         var pct3,pct4,pct5=0;
         var i, i1;
-        var finished=false;
-        
+        if (window.confirm('This scratch off contains loud sound when the gender is revealed. Do you want to continue with sound? (Ok:with sound, Cancel:without sound')) {
+            nosound=false;
+          } else {
+            nosound=true;
+        }
+        //var surname = searchParams.get('surname');
         // called each time a scratcher loads
         function onScratcherLoaded(ev) {
+            
             scratcherLoadedCount++;
             $("table1").width($(window).width());
             if (scratcherLoadedCount == scratchers.length) {
