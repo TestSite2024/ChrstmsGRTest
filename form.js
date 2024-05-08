cb41d1f9ef24590782a862c002ef479bf6d21e07
aykutkaraca <karaca.aykut@gmail.com>
2024-05-08 22:33:50 +0300

    format updates

--- form.js
+++ form.js
@@ -3,10 +3,16 @@ var inputLtc = document.getElementById("surname");
 var add = "https://aykutkaraca.github.io/GenderReveal/index.html?surname="
 inputLtc.addEventListener('keyup',OnKeyUp);
 var inputBtc = document.getElementById("address");
+inputBtc.value = add;
 var constantNumber = 2;
 
 function OnKeyUp(e) {
    var result = inputLtc.value;
-   inputBtc.value = (result) ? add + result : '';
+   if (result){
+      result = add + result.charAt(0).toUpperCase() + result.slice(1);
+   } else {
+      result=add;
+   }
+   inputBtc.value = result;
 };
 });
