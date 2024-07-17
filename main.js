39f9052e5c2910ba30d26ee2a20af8da76e2c541
aykutkaraca <karaca.aykut@gmail.com>
2024-07-17 16:07:07 +0300

    pumpkin confetti

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
  *
  * depends on jQuery>=1.7
  */
+var pumpkin;
 
 (function() {
     /**
@@ -120,14 +121,14 @@
         // do this for 10 seconds
         var duration = 10 * 1000;
         var end = Date.now() + duration;
-        var defaults = { startVelocity: 10, spread: 360, ticks: 70, zIndex: 0 };
+        var defaults = { scalar:2, startVelocity: 10, spread: 360, ticks: 50, zIndex: 0 };
         var particleCount = 5 ;
         (function frame() {
         // launch a few confetti from the left edge
-        confetti({...defaults, particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 }, colors: ['#FFFFFF']}
+        confetti({...defaults, particleCount, shapes: [pumpkin], origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },  colors: ['#ff9a00', '#ff7400', '#ff4d00']}
         );
         // and launch a few from the right edge
-        confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },colors: ['#FFFFFF']}
+        confetti({ ...defaults, particleCount, shapes: [pumpkin], origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 }, colors: ['#ff9a00', '#ff7400', '#ff4d00']}
         );
 
         // keep going until we are out of time
@@ -220,7 +221,13 @@
             $("#baby").text('the baby!');
             surname="the";
         }
-
+        $(document).ready(function(){
+            // function scope wavesurfer
+            pumpkin = confetti.shapeFromPath({
+                path: 'M449.4 142c-5 0-10 .3-15 1a183 183 0 0 0-66.9-19.1V87.5a17.5 17.5 0 1 0-35 0v36.4a183 183 0 0 0-67 19c-4.9-.6-9.9-1-14.8-1C170.3 142 105 219.6 105 315s65.3 173 145.7 173c5 0 10-.3 14.8-1a184.7 184.7 0 0 0 169 0c4.9.7 9.9 1 14.9 1 80.3 0 145.6-77.6 145.6-173s-65.3-173-145.7-173zm-220 138 27.4-40.4a11.6 11.6 0 0 1 16.4-2.7l54.7 40.3a11.3 11.3 0 0 1-7 20.3H239a11.3 11.3 0 0 1-9.6-17.5zM444 383.8l-43.7 17.5a17.7 17.7 0 0 1-13 0l-37.3-15-37.2 15a17.8 17.8 0 0 1-13 0L256 383.8a17.5 17.5 0 0 1 13-32.6l37.3 15 37.2-15c4.2-1.6 8.8-1.6 13 0l37.3 15 37.2-15a17.5 17.5 0 0 1 13 32.6zm17-86.3h-82a11.3 11.3 0 0 1-6.9-20.4l54.7-40.3a11.6 11.6 0 0 1 16.4 2.8l27.4 40.4a11.3 11.3 0 0 1-9.6 17.5z',
+                matrix: [0.020491803278688523, 0, 0, 0.020491803278688523, -7.172131147540983, -5.9016393442622945]
+              });
+        });
         document.getElementById('intro').innerHTML= "This is a gender reveal scratch off for <strong>" + surname + "</strong> family. It contains sound when the gender is revealed. Do you want to continue with sound?";
         document.getElementById('id01').style.display='block';
         $('.nosoundbtn').on("click", function (e) {
