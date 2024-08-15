8dc70efbba410b9ec067cf9592ac1f248b4e0630
aykutkaraca <karaca.aykut@gmail.com>
2024-08-15 20:33:45 +0300

    removed console.log

--- main.js
+++ main.js
@@ -289,7 +289,6 @@ var pct =new Array(9);
         document.addEventListener(
             "visibilitychange",
              function(evt) {
-                console.log("page hidden")
               if (document.visibilityState != "visible") {
                 soundHandle.pause();
                 soundHandle.currentTime=0;              }
