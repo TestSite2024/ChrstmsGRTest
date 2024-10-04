1085263a6c20213fa62650ca84896f05e87d78b0
aykutkaraca <karaca.aykut@gmail.com>
2024-10-04 12:32:23 +0300

    Corrected bold text in modal

--- main.js
+++ main.js
@@ -260,6 +260,7 @@ var pct =new Array(9);
         else {
             $("#baby").text('the baby!');
             surname="the";
+            document.getElementById('surname').style.fontWeight="normal";
         }
        /* $(document).ready(function(){
             // function scope wavesurfer
@@ -268,7 +269,9 @@ var pct =new Array(9);
                 matrix: [0.49984879,0,0,0.49984879,-41.861374,-62.27708]
                         });
         }); */
-        document.getElementById('intro').innerHTML= "This is a gender reveal scratch off for <strong>" + surname + "</strong> family. It contains sound when the gender is revealed. Do you want to continue with sound?";
+        //document.getElementById('intro').innerHTML= "This is a gender reveal scratch off for <strong>" + surname + "</strong> family. It contains sound when the gender is revealed. Do you want to continue with sound?";
+        document.getElementById('surname').innerHTML= surname;
+
         document.getElementById('id01').style.display='block';
         $('.nosoundbtn').on("click", function (e) {
             document.getElementById('id01').style.display='none';
